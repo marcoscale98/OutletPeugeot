759bc68fc9091407bc5050b6e09938d4c9ce71fa
Marco Scalerandi <marcoscale98@hotmail.com>
2020-07-08 15:53:20 +0200

    modificate print

--- src/__init__.py
+++ src/__init__.py
@@ -65,19 +65,20 @@ def ha_optional_giusti(nuova_auto: dict, optional_che_vorrei):
             return False
     return True
 
-def get_new_car():
+def get_new_car(cars_json='cars.json'):
     global list_auto_old, list_auto_new, link
     print("Nuova ricerca auto")
     # GET INFO
     box_auto = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]")
     automobili = box_auto.find_elements_by_class_name('result')
+    print("Trovate", str(len(automobili)),"auto da analizzare")
     list_auto_old = {}
     list_auto_new = {}
     try:
-        with open('cars.json', 'r', encoding='UTF-8') as reader:
+        with open(cars_json, 'r', encoding='UTF-8') as reader:
             list_auto_old = json.load(reader)
     except FileNotFoundError:
-        with open('cars.json','x',encoding='UTF-8') as writer:
+        with open(cars_json, 'x', encoding='UTF-8') as writer:
             writer.write(json.dumps({}))
     # pulsanti_pagina = box_auto.find_element_by_tag_name('footer').find_element_by_class_name('pagination').find_element_by_tag_name('ul').find_elements_by_tag_name('il')
     # altre_pagine = pulsanti_pagina[-1].find_element_by_tag_name('span')
@@ -94,8 +95,6 @@ def get_new_car():
             continue
         link = info_box.find_element_by_tag_name('h2').find_element_by_tag_name('a').get_attribute('href')
         nome = info_box.find_element_by_tag_name('h2').find_element_by_tag_name('a').text
-        print(link)
-        print(nome)
         # click per aprire lista optional
         info_box.find_elements_by_tag_name('p')[1].find_element_by_tag_name('a').click()
         time.sleep(2)
@@ -108,36 +107,46 @@ def get_new_car():
         nuova_auto = {'nome': nome, 'optional': optional}
         if ha_optional_giusti(nuova_auto, optional_desiderati.copy()):
 
-            print('Ho trovato questa auto', nuova_auto)
+            print(n_auto, '- Ho trovato questa auto come la vuoi tu', nuova_auto)
             controlla_novita(nuova_auto)
         # print('prossima pagina')
         # pulsanti_pagina[-2].find_element_by_tag_name('a').click()
         #time.sleep(2)
         # /div/div[2]/footer/div[2]/ul/li[4]/a
 
-    with open('cars.json','w',encoding='UTF-8') as writer:
-        writer.write(json.dumps(list_auto_new))
+    with open(cars_json, 'w', encoding='UTF-8') as writer:
+        writer.write(json.dumps(list_auto_new, indent=3))
+    print("Fine ricerca")
 
 
-def start_new_search():
+def start_new_search(cars_json):
     global driver
     try:
         with Chrome() as driver:
             driver.get(sito)
-            driver.maximize_window()
+            #driver.minimize_window()
 
-            settings()
-            get_new_car()
+            try:
+                settings()
+                get_new_car(cars_json)
+            except Exception as e:
+                print(e)
             # time.sleep(120)
     except socket.gaierror:
         print("Errore di connessione")
+
+#start
+with open(r'D:\\Marco\\Universita\\Progetti\\OutletPeugout\\stderr.txt','w') as init:
+    print(file=init)
 try:
     sito = "https://webstore.peugeot.it/Cerca-per-categorie?lat=45.0607417&lng=7.528754299999999&LocationL=10098%20Rivoli%20TO%2C%20Italia&etd=0&mbd=1PP2S5P00000;&nbDisMax=20&GrTransmissionType=BVA00006&GrTransmissionTypeL=%20Automatico%20a%208%20Rapporti%20S&S&GrGrade=10000020;10001074&GrGradeL=&dst=100"
     optional_desiderati = ['Drive Assist Plus','Sensori di Parcheggio Anteriori con Visiopark 180']
+    cars_json=r'D:\\Marco\\Universita\\Progetti\\OutletPeugout\\src\\cars.json'
+    time.sleep(10)
     while True:
-        start_new_search()
+        start_new_search(cars_json)
         time.sleep(60*30)
 except Exception as e:
-    with open('stderr.txt','w') as error:
+    with open(r'D:\\Marco\\Universita\\Progetti\\OutletPeugout\\stderr.txt','w') as error:
         print(e,file=error)
-        raise
+    raise
